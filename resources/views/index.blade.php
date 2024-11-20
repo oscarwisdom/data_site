89f3aa360b1c8d522d69bf0040d2b1c6f8b85504
omekeuchechi <omekejoseph97@gmail>
2024-11-20 16:14:09 +0100

    add

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,5 +20,6 @@
 </div>
 
 @include('includes.service')
+@include('includes.swipper')
 @include('includes.footer')
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href={{ asset('assset/fontawesome-free-6.5.2-web/css/all.css') }}>
     <link rel="stylesheet" href={{ asset('css/style3.css') }}>
     <link rel="stylesheet" href={{ asset('css/style4.css') }}>
+    <link rel="stylesheet" href={{ asset('css/swipper.css') }}>
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
@@ -23,25 +24,28 @@
     </div>
 
     <script src="{{ asset('assset/js/jquery.min.js') }}"></script>
-
+    
     <script>
+        // import linkWrappe from 'assset/js/footer.js';
+
         const regBtnHide = document.querySelector('.reg');
         const harmBurger = document.querySelector('.fa-bars');
-
+        
         regBtnHide.addEventListener('click', () => regShow())
-
+        
         harmBurger.addEventListener('click', () => navList())
 
         function regShow() {
             $('.reg-btn').toggle();
         }
-
+        
         function navList() {
             $('.ul').toggle();
         }
-
         
-    </script>
+        </script>
+        <script src="{{ asset('assset/js/footer.js') }}"></script>
+        <script src="{{ asset('assset/js/swipper.js') }}"></script>
 </body>
 </html>
 
